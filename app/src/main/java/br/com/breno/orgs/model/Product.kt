d31515ba85b61e20cfe8cac64d1392c21ef9860d
Jeyzonn Aragão <breno.m.aragao@gmail.com>
2023-04-23 20:34:09 -0300

    chorë: changed Product pacelizable

--- app/src/main/java/br/com/breno/orgs/model/Product.kt
+++ app/src/main/java/br/com/breno/orgs/model/Product.kt
@@ -1,11 +1,14 @@
 package br.com.breno.orgs.model
 
+import android.os.Parcelable
 import java.math.BigDecimal
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Product(
     val image: String? = null,
     val name: String,
     val description: String,
     val value: BigDecimal,
-)
+) : Parcelable
 
