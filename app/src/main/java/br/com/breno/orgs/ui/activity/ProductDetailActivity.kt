51d995e3a5784f656ee30bf574499d3f8ab2f3dc
Jeyzonn Aragão <breno.m.aragao@gmail.com>
2023-05-02 21:17:59 -0300

    feat: created product detail

--- app/src/main/java/br/com/breno/orgs/ui/activity/ProductDetailActivity.kt
+++ app/src/main/java/br/com/breno/orgs/ui/activity/ProductDetailActivity.kt
@@ -1,7 +1,13 @@
 package br.com.breno.orgs.ui.activity
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import br.com.breno.orgs.R
+import br.com.breno.orgs.database.OrgsDatabase
 import br.com.breno.orgs.databinding.ActivityProductDetailBinding
 import br.com.breno.orgs.extensions.formatToBrazilCurrency
 import br.com.breno.orgs.extensions.tryLoadImage
@@ -10,6 +16,7 @@ import br.com.breno.orgs.utils.KEY_PRODUCT
 
 class ProductDetailActivity: AppCompatActivity() {
 
+    private lateinit var product: Product
     private val binding by lazy { ActivityProductDetailBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +25,34 @@ class ProductDetailActivity: AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_product_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(::product.isInitialized) {
+            val db = OrgsDatabase.getInstance(this)
+            val productDao = db.productDao()
+            when(item.itemId) {
+                R.id.menu_product_detail_edit -> {
+                    Intent(this, ProductFormActivity::class.java).apply {
+                        putExtra(KEY_PRODUCT, product)
+                        startActivity(this)
+                    }
+                }
+                R.id.menu_product_detail_delete -> {
+                    productDao.deleteItem(product)
+                    finish()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun tryLoadProduct() {
         intent.getParcelableExtra<Product>(KEY_PRODUCT)?.let { loadedProduct ->
+            product = loadedProduct
             fillFields(loadedProduct)
         } ?: finish()
 
@@ -33,5 +66,4 @@ class ProductDetailActivity: AppCompatActivity() {
             detailValue.text = loadedProduct.value.formatToBrazilCurrency()
         }
     }
-
 }
